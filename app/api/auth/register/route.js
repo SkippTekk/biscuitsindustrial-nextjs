238942f0ad076416c37df622d2b56b8a103f3036
xylr <andrewquick.dev@gmail.com>
2023-08-08 18:45:30 -0400

    Updating prisma schemas

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -5,7 +5,7 @@ import sendEmail from "../../../../components/mail/mailing";
 import bcrypt from "bcrypt";
 
 export const POST = async (request) => {
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient("biscuits");
 
   const { username, email, password } = await request.json();
 
